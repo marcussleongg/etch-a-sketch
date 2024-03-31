82a49f9c08b320c26759c21f824a12e32016ca94
Marcus Leong <marcusleongjx@gmail.com>
2024-03-31 11:26:37 +0800

    Add function to remove initial grid

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 const container = document.querySelector("#container");
 
-function createRow() {
-    let row = document.createElement("div");
-    row.classList.toggle("row");
-    let i = 0;
-    while (i<16) {
-        let square = document.createElement("div");
-        square.classList.toggle("square");
-        row.appendChild(square);
+function createGrid(gridNum) {
+    function createRow() {
+        let row = document.createElement("div");
+        row.classList.toggle("row");
+        let i = 0;
+        while (i<gridNum) {
+            let square = document.createElement("div");
+            square.classList.toggle("square");
+            row.appendChild(square);
+            i++;
+        }
+        container.appendChild(row);
+    }
+    
+    let i = 0
+    while (i<gridNum) {
+        createRow();
         i++;
     }
-    container.appendChild(row);
+
+    let squares = document.querySelectorAll(".square");
+    for (let square of squares) {
+        square.addEventListener('mouseenter', () => {square.classList.add("hovered")})
+    }
 }
 
-let i = 0
-while (i<16) {
-    createRow();
-    i++;
-}
-
-let squares = document.querySelectorAll(".square");
-for (let square of squares) {
-    square.addEventListener('mouseenter', () => {square.classList.add("hovered")})
-}
+createGrid(16);
 
 const button = document.querySelector("#btn");
 button.addEventListener('click', (num) => {
     num = prompt("Please enter the number of squares you want the grid to have (maximum is 100)");
+    while (container.hasChildNodes()) {
+        container.firstChild.remove();
+    }
+    createGrid(num);
 });
